870a2bdf7cdc295ce5f0399ce90ee5cc4b920204
martijn <tijntje_7@msn.com>
2014-09-27 15:22:08 +0200

    hljs is now a global inside the package

--- package.js
+++ package.js
@@ -2,15 +2,17 @@
 
 Package.describe({
   summary: "A markdown parser and compiler. Built for speed.",
-  version: "0.3.5",
+  version: "0.3.7",
   name: "azeirah:marked",
   git: "https://github.com/azeirah/meteor-marked.git"
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom("METEOR@0.9.1");
+  api.versionsFrom("METEOR@0.9.0");
   api.use("templating", "client", {weak: true});
 
   api.add_files("marked/lib/marked.js");
+  api.add_files("highlightjs/highlight.pack.js");
+  api.add_files("highlightjs/zenburn.css");
   api.add_files('template-integration.js', 'client');
 });
